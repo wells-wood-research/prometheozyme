b3489a544759c95c284c8e396cc12ae47517faed
Marta Chronowska <mchrnwsk@gmail.com>
2025-06-13 10:53:12 +0100

    Remove unused functions

--- drOrca.py
+++ drOrca.py
@@ -7,6 +7,8 @@ class FilePath:
 class DirectoryPath:
     pass
 
+# from https://github.com/wells-wood-research/qmmm/blob/559b8ddf88eaab55f138e4ad2bdc04c3016f1f2d/scripts/drOrca.py
+
 def write_input(f, inputFormat, moleculeInfo, inputFile):
     qmCharge = moleculeInfo["charge"]
     qmMultiplicity = moleculeInfo["multiplicity"]
@@ -208,6 +210,7 @@ def write_title(f, title):
     f.write("\n")
 
 ########################################
+
 def modify_orca_input(input_file):
     # Read the input file
     with open(input_file, 'r') as f:
@@ -276,16 +279,7 @@ def modify_orca_input(input_file):
     # Write back to the input file
     with open(input_file, 'w') as f:
         f.writelines(lines)
-########################################
-def run_orca(orcaInput, orcaOutput, config):
-    orcaExe = config["pathInfo"]["orcaExe"]
-    orcaCommand = [orcaExe, orcaInput]
-    with open(orcaOutput, 'w') as output_file:
-        try:
-            call(orcaCommand, stdout=output_file, stderr=output_file)
-        except Exception as e:
-            raise(e)
-########################################        
+   
 def make_orca_input(orcaInput: FilePath,
                     title: str,
                     qmMethod: str,
@@ -327,159 +321,3 @@ def make_orca_input(orcaInput: FilePath,
             write_docker_block(f, docker)
         write_input(f, inputFormat, moleculeInfo, inputFile)
     return orcaInput
-########################################
-def make_orca_input_qmmm_singlepoint(inputXyz: FilePath,
-                                outDir: DirectoryPath,
-                                moleculeInfo: dict,
-                                qmMethod: str,
-                                qmAtoms: str,
-                                parameterFile: FilePath) -> FilePath:
-    ## write QM/MM single-point orca input file
-    charge = moleculeInfo["charge"]
-    multiplicity = moleculeInfo["multiplicity"]
-    qmmmSinglepointOrcaInput = p.join(outDir, f"QMMM_orca_sp.inp")
-    with open(qmmmSinglepointOrcaInput, "w") as f:
-        f.write(" # --------------------------------- #\n")
-        f.write(" #  QM/MM Single-Point Energy        #\n")
-        f.write(" # --------------------------------- #\n")  
-        ## simpleinput line for method and QMMM flag      
-        f.write(f"! QMMM {qmMethod} SP\n")
-        ## qmmmm options
-        f.write("%qmmm\n")
-        f.write(f"{' '*4}QMAtoms {qmAtoms} end\n")
-        f.write(f"{' '*4}ORCAFFFilename \"{parameterFile}\"\n")
-        f.write("end\n")
-        f.write(f"*xyzfile {charge} {multiplicity} {inputXyz}\n")
-    return qmmmSinglepointOrcaInput
-########################################
-def make_orca_input_for_solvator(inputXyz: FilePath,
-                                  outDir: DirectoryPath,
-                                    moleculeInfo: dict,
-                                      qmMethod: str,
-                                        solvationMethod: str,
-                                          nWaters: int) -> FilePath:
-    """
-    Creates and ORCA input file to run the SOLVATOR protocol
-    """
-    ## write GOAT orca input file
-    charge = moleculeInfo["charge"]
-    multiplicity = moleculeInfo["multiplicity"]
-    solvatorOrcaInput = p.join(outDir, f"SOLVATOR_orca.inp")
-    with open(solvatorOrcaInput, "w") as f:
-        f.write(" # --------------------------------- #\n")
-        f.write(" #  SOLVATOR solvent addition        #\n")
-        f.write(" # --------------------------------- #\n")
-        ## method and solvation
-        f.write(f"!{qmMethod} {solvationMethod}\n")
-        ## solvator params
-        f.write(f"%SOLVATOR\n")
-        f.write(f"{' '*4}NSOLV {nWaters}\n")
-        f.write(f"{' '*4}CLUSTERMODE STOCHASTIC\n")
-        f.write("END\n")
-        ## geom, charge, multiplicity
-        f.write(f"*xyzfile {charge} {multiplicity} {inputXyz}\n")
-    return solvatorOrcaInput
-########################################
-def write_goat_input(conformerDir, cappedXyz, config):
-    ## write GOAT orca input file
-    charge = config["moleculeInfo"]["charge"]
-    multiplicity = config["moleculeInfo"]["multiplicity"]
-    goatOrcaInput = p.join(conformerDir, f"GOAT_orca.inp")
-    with open(goatOrcaInput, "w") as f:
-        f.write(" # --------------------------------- #\n")
-        f.write(" #  GOAT conformation generation     #\n")
-        f.write(" # --------------------------------- #\n")
-        f.write("!XTB2 GOAT\n")
-        f.write("%PAL NPROCS 16 END\n")  ##TODO: work out how to use more cores or have a stable way of inputting this
-        f.write("%GOAT\n")
-        f.write("\tMAXITERMULT 1\n")            ## only do one round (save some time)
-        f.write("\tFREEZEAMIDES TRUE\n")           ## freeze amides
-        f.write("\tFREEZECISTRANS TRUE\n ")         ## freeze cis-trans bonds
-        f.write("END\n")
-        f.write(f"*xyzfile {charge} {multiplicity} {cappedXyz}\n")
-    return goatOrcaInput
-########################################
-def make_orca_input_for_opt(inputXyz, outDir, moleculeInfo, qmMethod, solvationMethod):
-    ## unpack moleculeInfo
-    charge = moleculeInfo["charge"]
-    multiplicity = moleculeInfo["multiplicity"]
-    ## create orca input file
-    orcaInputFile = p.join(outDir, "orca_opt.inp")
-    with open(orcaInputFile, "w") as f:
-        f.write(" # --------------------------------- #\n")
-        f.write(" #  Geometry Optimisation            #\n")
-        f.write(" # --------------------------------- #\n")
-        ## METHOD
-        if solvationMethod is None:
-            f.write(f"! {qmMethod} Opt\n")
-        else:
-            f.write(f"! {qmMethod} {solvationMethod} Opt\n") 
-        ## GEOMETRY
-        f.write(f"*xyzfile {charge} {multiplicity} {inputXyz}\n\n")
-
-    return orcaInputFile
-###################################################################################
-def  make_orca_input_for_scan(inputXyz,
-                               outDir,
-                                 moleculeInfo,
-                                   qmMethod,
-                                     solvationMethod,
-                                       torsionIndexes,
-                                         scanAngles):
-    ## unpack moleculeInfo
-    charge = moleculeInfo["charge"]
-    multiplicity = moleculeInfo["multiplicity"]
-    ## create orca input file
-    orcaInputFile = p.join(outDir, "orca_scan.inp")
-    with open(orcaInputFile, "w") as f:
-        f.write(" # --------------------------------- #\n")
-        f.write(" #  Torsion Scan                     #\n")
-        f.write(" # --------------------------------- #\n")
-        ## METHOD
-        if solvationMethod is None:
-            f.write(f"! {qmMethod} Opt\n")
-        else:
-            f.write(f"! {qmMethod} {solvationMethod} Opt\n") 
-        ## SCAN 
-        if not scanAngles is None:
-            f.write("%geom Scan\n")
-            torsionText: str = f"D {' '.join(map(str, torsionIndexes))} = {scanAngles}\n"
-            f.write(torsionText)
-            f.write("end\n")
-            f.write("end\n")
-
-        ## GEOMETRY
-        f.write(f"*xyzfile {charge} {multiplicity} {inputXyz}\n\n")
-
-    return orcaInputFile
-###################################################################################
-def  make_orca_input_for_singlepoint(inputXyz, outDir, moleculeInfo, qmMethod, solvationMethod):
-    ## unpack moleculeInfo
-    charge = moleculeInfo["charge"]
-    multiplicity = moleculeInfo["multiplicity"]
-    ## create orca input file
-    orcaInputFile = p.join(outDir, "orca_sp.inp")
-    with open(orcaInputFile, "w") as f:
-        f.write(" # --------------------------------- #\n")
-        f.write(" #  Single Point Calculation         #\n")
-        f.write(" # --------------------------------- #\n")
-        ## METHOD
-        if solvationMethod is None:
-            f.write(f"! {qmMethod} SP\n")
-        else:
-            f.write(f"! {qmMethod} {solvationMethod} SP\n") 
-        ## GEOMETRY
-        f.write(f"*xyzfile {charge} {multiplicity} {inputXyz}\n\n")
-
-    return orcaInputFile
-###################################################################################
-def did_orca_finish_normallly(orcaOut):
-    with open(orcaOut, "r") as f:
-        lines = f.readlines()
-        for line in reversed(lines):
-            if "****ORCA TERMINATED NORMALLY****" in line:
-                return True
-            elif "ORCA finished with an error in the energy calculation" in line:
-                return False
-    return False
-###################################################################################
