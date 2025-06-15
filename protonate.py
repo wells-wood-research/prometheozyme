147ee50eb6eeee3b4794133e7c861dd22a50eb0a
Marta Chronowska <mchrnwsk@gmail.com>
2025-06-15 21:44:52 +0100

    Separate protonation and deprotonation into dedicated functions

--- protonate.py
+++ protonate.py
@@ -1,9 +1,11 @@
 from pymol import cmd
 import os
 
-def protonate(path=None, unprotonatedIdx=None):
+def protonate_all(path=None):
     # Clear all objects in PyMOL
     cmd.delete('all')
+
+    cmd.set("retain_order", "on")
 
     # Load your molecule
     cmd.load(path, 'mol')
@@ -11,16 +13,34 @@ def protonate(path=None, unprotonatedIdx=None):
     # Add hydrogens to the molecule
     cmd.h_add('mol')
 
-    # Define the list of atom indices (example: 10, 15, 20) to remove
+    # Save the fully protonated structure
+    outfile = f"{os.path.splitext(path)[0]}_prot.pdb"
+   
+    cmd.save(outfile, 'mol', state=0, format='pdb', quiet=0)
+    atom_count = cmd.count_atoms('mol')
+
+    # Delete the molecule to clean up
+    cmd.delete('mol')
+
+    return outfile, atom_count
+
+def deprotonate_selected(path=None, unprotonatedIdx=None):
+    # Clear all objects in PyMOL
+    cmd.delete('all')
+
+    # Load your molecule
+    cmd.load(path, 'mol')
+
+    # Remove hydrogens from specified atoms
     if unprotonatedIdx:
-        # Convert orca indices to a PyMOL selection string
-        pymol_indices = [x+1 for x in unprotonatedIdx]
+        # Convert 0-based indices to 1-based PyMOL indices
+        pymol_indices = [x + 1 for x in unprotonatedIdx]
         index_selection = '+'.join(str(i) for i in pymol_indices)
-        # Note: Use 'neighbor' to select hydrogens bonded to the target atoms
         cmd.remove(f'elem H and neighbor (index {index_selection})')
 
-    # Save the modified structure with hydrogens
-    outfile = f"{os.path.splitext(path)[0]}_Hs.pdb"
+    # Save the deprotonated structure
+    outfile = f"{os.path.splitext(path)[0]}_deprot.pdb"
+    cmd.set("retain_order", "on")
     cmd.save(outfile, 'mol', state=0, format='pdb', quiet=0)
     atom_count = cmd.count_atoms('mol')
 
